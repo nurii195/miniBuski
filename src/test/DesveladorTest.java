e413dbf11c546a6375ee882cdd2808e3d0960317
Doritas <alirivera25@hotmail.com>
2017-03-31 10:45:09 +0200

    Alicia. Iniciando los test de desvelador

---
+++ src/test/DesveladorTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DesveladorTest {
+
+	@Test
+	public void testDesvelador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDesvelarCasillas() {
+		fail("Not yet implemented");
+	}
+
+}
