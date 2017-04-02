ce03d2f1a074784bb5b2380d47893e081d941f90
Doritas <alirivera25@hotmail.com>
2017-04-02 19:25:08 +0200

    Alicia. test Iniciador

---
+++ src/test/IniciadorTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IniciadorTest {
+
+	@Test
+	public void testCrearTablero() {
+		fail("Not yet implemented");
+	}
+
+}
