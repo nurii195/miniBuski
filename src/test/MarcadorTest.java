49e33268aa2df35b5590bbe99008af5a6d2e9c0a
Doritas <alirivera25@hotmail.com>
2017-04-02 19:26:22 +0200

    Alicia. test Marcador

---
+++ src/test/MarcadorTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarcadorTest {
+
+	@Test
+	public void testMarcarCasilla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSincronizar() {
+		fail("Not yet implemented");
+	}
+
+}
