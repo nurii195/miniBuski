c5928e8e3f9ab0c9dddc868eb2cbb22292eb7d68
nurii195 <nurii195@hotmail.com>
2017-04-03 09:40:56 +0200

    nuria. he borrado el test desvelador, ya que era el constructor

--- src/test/DesveladorTest.java
+++ src/test/DesveladorTest.java
@@ -7,12 +7,6 @@ import org.junit.Test;
 public class DesveladorTest {
 
 	@Test
-	public void testDesvelador() {
-		int a;
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testDesvelarCasillas() {
 		fail("Not yet implemented");
 	}
