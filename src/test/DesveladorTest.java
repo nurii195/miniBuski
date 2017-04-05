7c8d627f9943575259478f6ded09f6a31de981b7
Doritas <alirivera25@hotmail.com>
2017-04-05 09:16:06 +0200

    Alicia. test desvelador

--- src/test/DesveladorTest.java
+++ src/test/DesveladorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import control.Desvelador;
+import control.Iniciador;
 import modelo.Casilla;
 import modelo.Coordenada;
 import modelo.Tablero;
@@ -13,14 +14,23 @@ public class DesveladorTest {
 
 	@Test
 	public void testDesvelarCasillas() {
+		
 		Coordenada coord=new Coordenada(0, 0);
-		Tablero instancia=new Tablero(3, 3);
-		Casilla[][] casillas = instancia.getCasillas();
-		Desvelador desvelador=new Desvelador(instancia);
+		Casilla casilla=new Casilla();
+		
+		
+		Iniciador iniciador=new Iniciador();
+		Tablero tablero = iniciador.crearTablero(3, 3, 2);
+		Desvelador desvelar=new Desvelador(tablero);
+		
+		int minasAlrededor = casilla.getMinasAlrededor();
+		casilla.setMinas(true);
+			
+		//desvelar.desvelarCasillas(coord.getX(), coord.getY());
+		
+		assertEquals(minasAlrededor, 0);
+
 	
-		
-		
-		desvelador.desvelarCasillas(coord.getX(), coord.getY());
 	}
 
 }
